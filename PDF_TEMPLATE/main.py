49f1f5f4b0c30c6e931c5bb606dd04bcdb8bf99a
Bakare Damilola <bdamilola00@gmail.com>
2023-08-15 02:52:59 +0100

    separate logic into functions

--- PDF_TEMPLATE/main.py
+++ PDF_TEMPLATE/main.py
@@ -1,20 +1,40 @@
 import pandas as pd
 from fpdf import FPDF
 
-
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv("topic.csv")
 
-for index, row in df.iterrows():
-    num_of_pages = row["Pages"] - 1
-    pdf.add_page()
+
+def header(title):
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.cell(w=0, h=12, txt=title, align="L", ln=1)
     pdf.line(10.5, 20.5, 199.5, 20.5)
-    for i in range(num_of_pages):
+
+
+def footer(title: str, line: int):
+    pdf.ln(line)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=title, align="R")
+
+
+def main():
+    for index, row in df.iterrows():
+        text = row["Topic"]
+        num_of_pages = row["Pages"] - 1
+        # HEADER
         pdf.add_page()
+        header(text)
+        # FOOTER
+        footer(text, 265)
+        for i in range(num_of_pages):
+            pdf.add_page()
+            footer(text, 275)
+
+    pdf.output("output.pdf")
 
 
-pdf.output("output.pdf")
-
+if __name__ == '__main__':
+    main()
