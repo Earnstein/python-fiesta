d287cb75f24f975a65b17d7b8fd5797bba3bd167
Bakare Damilola <bdamilola00@gmail.com>
2023-08-15 03:20:16 +0100

    add note lines

--- PDF_TEMPLATE/main.py
+++ PDF_TEMPLATE/main.py
@@ -13,6 +13,14 @@ def header(title):
     pdf.line(10.5, 20.5, 199.5, 20.5)
 
 
+def note_lines():
+    y_cor = 20.5
+    for i in range(28):
+        y_cor += 10
+        if y_cor < 290:
+            pdf.line(10.5, y_cor, 199.5, y_cor)
+
+
 def footer(title: str, line: int):
     pdf.ln(line)
     pdf.set_font(family="Times", style="I", size=8)
@@ -24,13 +32,19 @@ def main():
     for index, row in df.iterrows():
         text = row["Topic"]
         num_of_pages = row["Pages"] - 1
+
         # HEADER
         pdf.add_page()
         header(text)
+
+        # NOTE LINE
+        note_lines()
+
         # FOOTER
         footer(text, 265)
         for i in range(num_of_pages):
             pdf.add_page()
+            note_lines()
             footer(text, 275)
 
     pdf.output("output.pdf")
