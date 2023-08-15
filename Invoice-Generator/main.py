5510f68eb627d23b54804e2294725fa735f3e14b
Bakare Damilola <bdamilola00@gmail.com>
2023-08-15 15:46:25 +0100

    Add excel content into pdf files

--- Invoice-Generator/main.py
+++ Invoice-Generator/main.py
@@ -5,13 +5,25 @@ from pathlib import Path
 
 filepaths = glob.glob("Invoices/*.xlsx")
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_nr_list = filename.split("-")
-    invoice_nr = invoice_nr_list[0]
-    text = f"Invoices nr. {invoice_nr}"
+    invoice_nr, date = filename.split("-")
+
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=text, align="L", border=1)
+    pdf.cell(w=50, h=8, txt=f"Invoices nr. {invoice_nr}", align="L", ln=1)
+
+    pdf.set_font(family="Times", size=12, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", align="L", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=8)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
     pdf.output(f"PDFs/{filename}.pdf")
