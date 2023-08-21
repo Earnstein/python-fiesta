188203f1e4e3dbb9868ec0737b728ada95512103
Bakare Damilola <bdamilola00@gmail.com>
2023-08-21 11:51:44 +0100

    add temperature and date data for API from files.

--- hello_flask/main.py
+++ hello_flask/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import pandas as pd
-
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -12,11 +12,15 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temp = 23
+    station_variable = str(station).zfill(6)
+    filename = f"data_small/TG_STAID{station_variable}.txt"
+    formatted_date = datetime.strptime(date, "%Y%M%d").strftime("%Y-%M-%d")
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == formatted_date]["   TG"].squeeze()/10
     return {
         "station": station,
-        "date": date,
-        "temperature": temp
+        "date": formatted_date,
+        "temperature": temperature
     }
 
 
