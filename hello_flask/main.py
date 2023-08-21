035d0e8d54804d84045eae888dcaf61c926748a6
Bakare Damilola <bdamilola00@gmail.com>
2023-08-21 13:28:08 +0100

    add station table to index.html, built API logic  for a station and a year endpoint.

--- hello_flask/main.py
+++ hello_flask/main.py
@@ -1,27 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import pandas as pd
 from datetime import datetime
 
 app = Flask(__name__)
 
+DATA_DIRECTORY = "data_small"
+
+
+def load_station_data(filename, skip_rows):
+    return pd.read_csv(filename, skiprows=skip_rows)
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    stations_columns = ["STAID", "STANAME"]
+    stations = load_station_data(f"{DATA_DIRECTORY}/stations.txt", skip_rows=17)
+    stations_table = stations[stations_columns].to_html()
+    return render_template("index.html", data=stations_table)
+
+
+@app.route("/api/v1/<station>")
+def station(station):
+    station_variable = str(station).zfill(6)
+    filename = f"{DATA_DIRECTORY}/TG_STAID{station_variable}.txt"
+    df = load_station_data(filename, skip_rows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    station_data = df.to_dict(orient="records")
+    return jsonify(station_data)
 
 
 @app.route("/api/v1/<station>/<date>")
-def about(station, date):
+def stations(station, date):
     station_variable = str(station).zfill(6)
-    filename = f"data_small/TG_STAID{station_variable}.txt"
-    formatted_date = datetime.strptime(date, "%Y%M%d").strftime("%Y-%M-%d")
-    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
-    temperature = df.loc[df["    DATE"] == formatted_date]["   TG"].squeeze()/10
-    return {
+    filename = f"{DATA_DIRECTORY}/TG_STAID{station_variable}.txt"
+    df = load_station_data(filename, skip_rows=20)
+    formatted_date = datetime.strptime(date, "%Y%M%d").strftime("%Y-%m-%d")
+    temperature = df.loc[df["    DATE"] == formatted_date, "   TG"].squeeze() / 10
+    return jsonify({
         "station": station,
         "date": formatted_date,
         "temperature": temperature
-    }
+    })
+
+
+@app.route("/api/v1/year/<station>/<year>")
+def year(station, year):
+    station_variable = str(station).zfill(6)
+    filename = f"{DATA_DIRECTORY}/TG_STAID{station_variable}.txt"
+    df = load_station_data(filename, skip_rows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    filtered_data = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return jsonify(filtered_data)
 
 
 if __name__ == '__main__':
