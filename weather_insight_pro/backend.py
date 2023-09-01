92fed16ce527d97d2a5053d7f217a5a6f3eea21e
Bakare Damilola <bdamilola00@gmail.com>
2023-09-01 23:25:43 +0100

    implemented api for getting weather data.

--- weather_insight_pro/backend.py
+++ weather_insight_pro/backend.py
@@ -1,5 +1,7 @@
 import os
 import requests
+
+
 def get_data(days):
     dates = ["2020-10-08", "2020-10-13", "2020-10-15"]
     temperatures = [10, 11, 25]
@@ -7,13 +9,23 @@ def get_data(days):
     return dates, temperatures
 
 
-url = "https://api.openweathermap.org/data/2.5/forecast?"
-params = {
-    "lat": float(os.getenv("MY_LAT")),
-    "lon": float(os.getenv("MY_LONG")),
-    "appid": os.getenv("OWM_API_KEY")
-}
+def get_weather_data(city_name: str, forecast_days=None, kind=None):
+    num_of_weather_data = 8 * forecast_days
+    url = "https://api.openweathermap.org/data/2.5/forecast?"
+    params = {
+        "q": city_name,
+        "appid": os.getenv("OWM_API_KEY")
+    }
+    request = requests.get(url, params=params)
+    response = request.json()
+    response_1 = response['list']
+    weather_data = response_1[:num_of_weather_data]
+    return weather_data
 
-request = requests.get(url, params=params)
-response = request.json()
-print(response)
+
+if __name__ == "__main__":
+    a = get_weather_data("akure", 5, "temperature")
+    print(a)
+
+
+
