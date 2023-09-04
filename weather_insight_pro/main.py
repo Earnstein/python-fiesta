7fb5710f490c3e0ce66f8de2323499689167e8e5
Bakare Damilola <bdamilola00@gmail.com>
2023-09-04 20:15:31 +0100

    handled incorrect place error.

--- weather_insight_pro/main.py
+++ weather_insight_pro/main.py
@@ -4,27 +4,30 @@ from backend import get_weather_data
 
 st.title("Weather Forecast for the Next Days")
 
-place = st.text_input("Place: ")
+place = st.text_input("Place: ").title()
 day = st.slider("Forecast Days  ", min_value=1, max_value=5, help="Select the number of forecasted days")
 option = st.selectbox("Select the data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} forecast for the next {day} in {place}")
 
 if place:
-    weather_data = get_weather_data(city_name=place, forecast_days=day, kind=option)
-    dates = [entry["dt_txt"] for entry in weather_data]
+    try:
+        weather_data = get_weather_data(city_name=place, forecast_days=day, kind=option)
+        dates = [entry["dt_txt"] for entry in weather_data]
 
-    if option == "Temperature":
-        temperature_data = [entry["main"]["temp"] for entry in weather_data]
-        figure = px.line(x=dates, y=temperature_data, labels={"x": "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
+        if option == "Temperature":
+            temperature_data = [entry["main"]["temp"] for entry in weather_data]
+            figure = px.line(x=dates, y=temperature_data, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-    elif option == "Sky":
-        sky_conditions = {
-            "Clear": "assets/clear.png",
-            "Clouds": "assets/cloud.png",
-            "Rain": "assets/rain.png",
-            "Snow": "assets/snow.png"
-        }
-        sky_data = [entry["weather"][0]["main"] for entry in weather_data]
-        image_paths = [sky_conditions[key] for key in sky_data]
-        st.image(image_paths, width=115)
+        elif option == "Sky":
+            sky_conditions = {
+                "Clear": "assets/clear.png",
+                "Clouds": "assets/cloud.png",
+                "Rain": "assets/rain.png",
+                "Snow": "assets/snow.png"
+            }
+            sky_data = [entry["weather"][0]["main"] for entry in weather_data]
+            image_paths = [sky_conditions[key] for key in sky_data]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.info(f"{place} does not exists")
