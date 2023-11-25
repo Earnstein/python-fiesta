1595ec1474175f432536ef9594318b42ab8aca3b
Bakare Damilola <bdamilola00@gmail.com>
2023-11-25 21:36:44 +0100

    refactored code

--- UpdateFlow/main.py
+++ UpdateFlow/main.py
@@ -9,7 +9,7 @@ ENDPOINT = "https://newsapi.org/v2/everything"
 HEADER = {"X-Api-Key": API_KEY}
 
 
-def get_news(topic: str, date=DATE, num_results=5):
+def get_news(topic: str, date, num_results=5):
     """Gets the specified number of most recent internship news from news_org"""
     params = {
         "q": topic,
@@ -23,30 +23,34 @@ def get_news(topic: str, date=DATE, num_results=5):
     return news_list
 
 
-articles = get_news(topic="internships", date="2023-08-01")
-news_result = [
-    {
-        "title": article["title"],
-        "url": article["url"],
-        "description": article["description"]
-    } for article in articles
-]
+def main():
+    articles = get_news(topic="internships", date=DATE)
+    news_result = [
+        {
+            "title": article["title"],
+            "url": article["url"],
+            "description": article["description"]
+        } for article in articles
+    ]
 
-messages = []
-for news in news_result:
-    title = news["title"]
-    url = news["url"]
-    description = news["description"]
-    message = f"""
-    <html>
-        <body>
-            <h2>HEADLINE: {title}</h2>
-            <p>{description}</p>
-            <p>Read more: <a href="{url}">{url}</a></p>
-        </body>
-    </html>
-    """
-    messages.append(message)
+    messages = []
+    for news in news_result:
+        title = news["title"]
+        url = news["url"]
+        description = news["description"]
+        message = f"""
+        <html>
+            <body>
+                <h2>HEADLINE: {title}</h2>
+                <p>{description}</p>
+                <p>Read more: <a href="{url}">{url}</a></p>
+            </body>
+        </html>
+        """
+        messages.append(message)
 
-full_message = "\n".join(messages)
-send_email(full_message)
+    full_message = "\n".join(messages)
+    send_email(full_message)
+
+if __name__ == "__main__":
+    main()
