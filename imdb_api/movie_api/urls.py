2f57b029d83c96b47d35093fb3d8018ec7d519f8
Bakare Damilola <bdamilola00@gmail.com>
2023-12-04 21:58:56 +0100

    refactor: changed url to class based

--- imdb_api/movie_api/urls.py
+++ imdb_api/movie_api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 
 urlpatterns = [
-    path("movies/", views.httpGetMovies, name="movies"),
-    path("update/<int:pk>/", views.httpUpdateMovie, name="update"),
+    path("movies/", views.MovieListAV.as_view(), name="movies"),
+    path("update/<int:pk>/", views.MovieUpdateAV.as_view(), name="update"),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
