10bfa471f10071cb4c311d1390cb156bee8a7c1b
Bakare Damilola <bdamilola00@gmail.com>
2023-12-04 21:59:43 +0100

    refactor: change from serial to modelSerializer

--- imdb_api/movie_api/serlializers.py
+++ imdb_api/movie_api/serlializers.py
@@ -5,27 +5,3 @@ class MovieSerialiser(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ['id', 'name', 'description', 'active']
-
-
-
-# class MovieSerialiser(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     name  = serializers.CharField()
-#     description = serializers.CharField()
-#     active = serializers.BooleanField()
-
-#     def create(self,validated_data):
-#         """
-#         Create and return a new `Movie` instance, given the validated data.
-#         """
-#         return Movie.objects.create(**validated_data)
-    
-#     def update(self, instance, validated_data):
-#         """
-#         Update and return an existing `Movie` instance, given the validated data.
-#         """
-#         instance.name = validated_data.get('name', instance.name)
-#         instance.description = validated_data.get('description', instance.description)
-#         instance.active = validated_data.get('active', instance.active)
-#         instance.save()
-#         return instance
