040d89378c6acba6c1845e1a0761eed6aec6899e
earnstein <bdamilola00@gmail.com>
2024-06-01 18:30:38 +0100

    chore: added comments to urls

--- earneats/accounts/urls.py
+++ earneats/accounts/urls.py
@@ -1,17 +1,25 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+
+    # USER AUTHENTICATION URLS
     path("registerUser/", views.httpRegisterUser, name="registerUser"),
     path("registerVendor/", views.httpRegisterVendor, name="registerVendor"),
     path("login/", views.httpLogin, name="login"),
     path("logout/", views.httpLogout, name="logout"),
+
+    # USER ACCOUNT URLS
     path("", views.httpGetUserAccount),
     path("myAccount/", views.httpGetUserAccount, name="userAccount"),
     path("customerDashboard/", views.httpCustomerDashboard, name="customerDashboard"),
     path("vendorDashboard/", views.httpVendorDashboard, name="vendorDashboard"),
     path("activate/<uidb64>/<token>/", views.activate, name="activate"),
+
+    # FORGOT PASSWORD AND RESET PASSWORD
     path("forgotPassword/", views.httpForgotPassword, name="forgotPassword"),
     path("resetPasswordValidate/<uidb64>/<token>/", views.httpResetPasswordValidate, name="resetPasswordValidate"),
     path("resetPassword/", views.httpResetPassword, name="resetPassword"),
+
+    path("vendor/", include("vendor.urls")),
 ]
