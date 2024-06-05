4ec0592935ad91aae25a33fd46e85a1e82e9a92a
earnstein <bdamilola00@gmail.com>
2024-06-05 20:00:09 +0100

    add: user profile form

--- earneats/accounts/forms.py
+++ earneats/accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User, UserProfile
 
 
 class UserForm(forms.ModelForm):
@@ -7,15 +7,22 @@ class UserForm(forms.ModelForm):
     confirm_password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password']
+        fields = ["first_name", "last_name", "username", "email", "password"]
     
     def clean(self):
         cleaned_data = super(UserForm, self).clean()
-        password = cleaned_data.get('password')
-        confirm_password = cleaned_data.get('confirm_password')
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
 
         if password != confirm_password:
             raise forms.ValidationError(
                 "Password does not match"
             )
     
+
+class UserProfileForm(forms.ModelForm):
+    profile_picture = forms.ImageField(widget=forms.FileInput(attrs={"class": "btn btn-info"}))
+    cover_picture = forms.ImageField(widget=forms.FileInput(attrs={"class": "btn btn-info"}))
+    class Meta:
+        model = UserProfile
+        fields = ["profile_picture", "cover_picture",  "address_line_1", "address_line_2", "city", "state", "country", "pin_code", "latitude", "longitude"]
