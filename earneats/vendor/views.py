ff369560ae12f7716dc4a59e9db10e8dbbd034a5
earnstein <bdamilola00@gmail.com>
2024-06-05 20:14:23 +0100

    chore: change quotes to double

--- earneats/vendor/views.py
+++ earneats/vendor/views.py
@@ -10,24 +10,24 @@ from django.shortcuts import redirect
 def profileView(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     vendor = get_object_or_404(Vendor, user=request.user)
-    if request.method != 'POST':
+    if request.method == "POST":
         profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
         vendor_form = VendorForm(request.POST, request.FILES, instance=vendor)
         if profile_form.is_valid() and vendor_form.is_valid():
             profile_form.save()
             vendor_form.save()
-            messages.success(request, 'Profile updated successfully')
-            return redirect('profile')
+            messages.success(request, "Profile updated successfully")
+            return redirect("vendorProfile")
         else:
-            messages.error(request, 'Error updating profile')
+            messages.error(request, "Error updating profile")
             print(profile_form.errors, vendor_form.errors)
-            return redirect('profile')
+            return redirect("vendorProfile")
             
     profile_form = UserProfileForm(instance=profile)
     vendor_form = VendorForm(instance=vendor)
 
-    context = {'profile_form': profile_form, 'vendor_form': vendor_form, 'profile': profile, 'vendor': vendor}
-    return render(request, 'vendor/profile.html', context)
+    context = {"profile_form": profile_form, "vendor_form": vendor_form, "profile": profile, "vendor": vendor}
+    return render(request, "vendor/profile.html", context)
 
 
 
