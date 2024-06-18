06631b5073608850e9a60d2030b37f1fa78f4ead
earnstein <bdamilola00@gmail.com>
2024-06-18 01:59:12 +0100

    add: custom validator function for images

---
+++ earneats/accounts/form_validations.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import ValidationError
+from os import path
+
+def allow_images_only(value):
+    ext = path.splitext(value.name)[1]
+    valid_extensions = ['.jpg', '.png', '.jpeg']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Unsupported file extension.')
