2bfad974e95e52e8e1101f198a1cdb4b73821b7f
earnstein <bdamilola00@gmail.com>
2024-06-18 02:05:08 +0100

    chore: added validation on image

--- earneats/vendor/forms.py
+++ earneats/vendor/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from .models import Vendor
-
+from accounts.form_validations import allow_images_only
 
 class VendorForm(forms.ModelForm):
-    vendor_license = forms.ImageField(widget=forms.FileInput(attrs={"class": "btn btn-info"}))
+    vendor_license = forms.FileField(widget=forms.FileInput(attrs={"class": "btn btn-info"}), validators=[allow_images_only])
     class Meta:
         model = Vendor
         fields = ['vendor_name', 'vendor_license']
