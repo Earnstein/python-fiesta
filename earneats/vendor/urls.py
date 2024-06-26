285d0f6b95374c0849be85489ff53ca463917a93
earnstein <bdamilola00@gmail.com>
2024-06-26 19:22:11 +0100

    add: menu crud urls

--- earneats/vendor/urls.py
+++ earneats/vendor/urls.py
@@ -6,4 +6,9 @@ from accounts import views as acct_views
 urlpatterns = [
     path("profile/", views.profileView, name="vendorProfile"),
     path("", acct_views.httpVendorDashboard, name="vendorDashboard"),
+    path("menu/", views.menuView, name="menu"),
+    path("menu/create/", views.createMenu, name="createMenu"),
+    path("menu/<int:pk>/update/", views.updateMenu, name="updateMenu"),
+    path("menu/<int:pk>/delete/", views.deleteMenu, name="deleteMenu"),
+    path("menu/category/<int:pk>/", views.getMenuByCategory, name="getMenuByCategory"),
 ]
