3afd8bdb0b197f40ad8aa5fc74806de78a37a175
earnstein <bdamilola00@gmail.com>
2024-07-01 22:27:57 +0100

    feat: moved menu endpoint from vendors to menu url file

---
+++ earneats/menu/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from menu import views
+
+urlpatterns = [
+        # CATEGORIES URLS
+    path("", views.menuView, name="menu"),
+    path("category/<int:pk>/", views.getCategory, name="getCategory"),
+    path("category/create/", views.createCategory, name="createCategory"),
+    path("category/update/<int:pk>/", views.updateCategory, name="updateCategory"),
+    path("category/delete/<int:pk>/", views.deleteCategory, name="deleteCategory"),
+
+    # FOOD URLS
+    path("category/food/create/", views.createFood, name="createFood"),
+    path("category/food/update/<int:pk>/", views.updateFood, name="updateFood"), 
+    path("category/food/delete/<int:pk>/", views.deleteFood, name="deleteFood"),
+]
