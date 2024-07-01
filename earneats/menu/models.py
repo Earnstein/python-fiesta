b11b22e4673e1aba5121c2b2baf762688fac3fc7
earnstein <bdamilola00@gmail.com>
2024-07-01 21:43:09 +0100

    chore: formatted file

--- earneats/menu/models.py
+++ earneats/menu/models.py
@@ -15,8 +15,7 @@ class Category(models.Model):
 
     def clean(self):
         self.category_name = self.category_name.capitalize()
-    
-    
+     
     def __str__(self):
         return self.category_name
 
