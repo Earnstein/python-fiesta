291def500ad8444538baa8d1bb25696644d05b5c
earnstein <bdamilola00@gmail.com>
2024-07-02 22:33:42 +0100

    chore: register user model to admin with decorator

--- earneats/accounts/admin.py
+++ earneats/accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import User, UserProfile
 from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_active')
     ordering = ('-date_joined',)
@@ -10,5 +10,4 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ()
     fieldsets = ()
 
-admin.site.register(User, CustomUserAdmin)
 admin.site.register(UserProfile)
