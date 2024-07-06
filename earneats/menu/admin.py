f0a0bb78f34fdd8a55a2f1ba123afadaa9be768b
earnstein <bdamilola00@gmail.com>
2024-07-06 17:04:49 +0100

    refactor: added models to admin site using decorator

--- earneats/menu/admin.py
+++ earneats/menu/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 from .models import Category, FoodItem
 
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['vendor', 'category_name', 'slug', 'updated_at']
-    prepopulated_fields = {'slug': ('category_name',)}
+    prepopulated_fields = {'slug': ('category_name', 'vendor')}
     search_fields = ['category_name', 'vendor__vendor_name']
     ordering = ['category_name']
+
 
 @admin.register(FoodItem)
 class FoodItemAdmin(admin.ModelAdmin):
     list_display = ['vendor', 'food_title', 'slug', 'price', 'category', 'updated_at']
     list_filter = ['is_available']
-    prepopulated_fields = {'slug': ('food_title',)}
+    prepopulated_fields = {'slug': ('food_title', 'vendor')}
     search_fields = ['food_title', 'description', 'category__category_name']
     ordering = ['food_title', 'price']
