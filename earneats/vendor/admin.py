a7cb5ba1be57841f0eb46cab9423cf55515ddb97
earnstein <bdamilola00@gmail.com>
2024-07-06 17:07:26 +0100

    refactor: added models to admin site using decorator

--- earneats/vendor/admin.py
+++ earneats/vendor/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from vendor.models import Vendor
 
+@admin.register(Vendor)
 class VendorAdmin(admin.ModelAdmin):
-    list_display = ('user', 'vendor_name', 'is_approved', 'created_at')
-    list_display_links = ('user', 'vendor_name')
+    list_display = ['user', 'vendor_name', 'is_approved', 'created_at']
+    list_display_links = ['user', 'vendor_name']
+    prepopulated_fields = {'vendor_slug': ('vendor_name', 'user',)}
+    list_filter = ['is_approved', 'created_at']
+    search_fields = ['user__email', 'vendor_name']
+    ordering = ['-created_at']
 
 
-admin.site.register(Vendor, VendorAdmin)
