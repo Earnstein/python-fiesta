6566bd110617aa827f3a24bc5a44fda78e7b3612
earnstein <bdamilola00@gmail.com>
2024-07-11 02:05:38 +0100

    feat: added functionality for increasing and decreasing cart

--- earneats/earneats/static/js/custom.js
+++ earneats/earneats/static/js/custom.js
@@ -23,28 +23,72 @@ function onPlaceChanged() {
 
   geocoder.geocode({ address: address }, (results, status) => {
     if (status == google.maps.GeocoderStatus.OK) {
-      document.getElementById("id_latitude").value = results[0].geometry.location.lat();
-      document.getElementById("id_longitude").value = results[0].geometry.location.lng();
-      document.getElementById("id_address").value = results[0].formatted_address;
+      document.getElementById("id_latitude").value =
+        results[0].geometry.location.lat();
+      document.getElementById("id_longitude").value =
+        results[0].geometry.location.lng();
+      document.getElementById("id_address").value =
+        results[0].formatted_address;
     }
   });
 
   for (var i = 0; i < place.address_components.length; i++) {
-   for (var j = 0; j < place.address_components[i].types.length; j++) {
-     if (place.address_components[i].types[j] == "country") {
-       document.getElementById("id_country").value = place.address_components[i].long_name;
-     };
-     if (place.address_components[i].types[j] == "administrative_area_level_1") {
-       document.getElementById("id_state").value = place.address_components[i].long_name;
-     };
-     if (place.address_components[i].types[j] == "locality") {
-       document.getElementById("id_city").value = place.address_components[i].long_name;
-     };
-     if (place.address_components[i].types[j] == "postal_code") {
-       document.getElementById("id_pin_code").value = place.address_components[i].long_name;
-     }else {
-       document.getElementById("id_pin_code").value = "";
-     }
-   };
-  };
+    for (var j = 0; j < place.address_components[i].types.length; j++) {
+      if (place.address_components[i].types[j] == "country") {
+        document.getElementById("id_country").value =
+          place.address_components[i].long_name;
+      }
+      if (
+        place.address_components[i].types[j] == "administrative_area_level_1"
+      ) {
+        document.getElementById("id_state").value =
+          place.address_components[i].long_name;
+      }
+      if (place.address_components[i].types[j] == "locality") {
+        document.getElementById("id_city").value =
+          place.address_components[i].long_name;
+      }
+      if (place.address_components[i].types[j] == "postal_code") {
+        document.getElementById("id_pin_code").value =
+          place.address_components[i].long_name;
+      } else {
+        document.getElementById("id_pin_code").value = "";
+      }
+    }
+  }
 }
+
+$(document).ready(function () {
+  $(".add_to_cart").click(function (event) {
+    event.preventDefault();
+    food_id = $(this).data("id");
+    data = { food_id: food_id };
+    url = $(this).data("url");
+    $.ajax({
+      type: "GET",
+      url: url,
+      data: data,
+      success: function (data) {
+        console.log(data);
+      },
+    });
+  });
+});
+
+
+$(document).ready(function () {
+  $(".remove_from_cart").click(function (event) {
+    event.preventDefault();
+    food_id = $(this).data("id");
+    data = { food_id: food_id };
+    url = $(this).data("url");
+    $.ajax({
+      type: "GET",
+      url: url,
+      data: data,
+      success: function (data) {
+        console.log(data);
+      },
+    });
+  });
+});
