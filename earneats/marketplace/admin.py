55abf06470b0a3b4d979dc61915139bed64a5cfa
earnstein <bdamilola00@gmail.com>
2024-07-11 02:07:07 +0100

    feat: register cart model with admin

--- earneats/marketplace/admin.py
+++ earneats/marketplace/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cart
 
-# Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'fooditem', 'quantity', 'updated_at']
+    list_filter = ['user']
+    search_fields = ['user__username', 'fooditem__food_title']
+    ordering = ['fooditem']
