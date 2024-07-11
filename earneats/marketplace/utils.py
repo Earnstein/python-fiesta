635d0f5363645514276219e3ed3a9872617cae51
earnstein <bdamilola00@gmail.com>
2024-07-11 19:18:37 +0100

    add: get_total_cart_quantity util

--- earneats/marketplace/utils.py
+++ earneats/marketplace/utils.py
@@ -1,2 +1,9 @@
+from .models import Cart
+
 def is_ajax(request):
     return request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+
+def get_total_cart_quantity(user):
+    cart_items = Cart.objects.filter(user=user)
+    total_quantity = sum(cart_item.quantity for cart_item in cart_items)
+    return total_quantity
