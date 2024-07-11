59677882389e01394b63353a41b819d3a959af5b
earnstein <bdamilola00@gmail.com>
2024-07-11 02:09:50 +0100

    feat: added add/remove to/from cart view

--- earneats/marketplace/views.py
+++ earneats/marketplace/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404
+from django.db.models import Prefetch
+from django.http import HttpResponse, JsonResponse
 from vendor.models import Vendor
 from menu.models import Category, FoodItem
-from django.db.models import Prefetch
+from .models import Cart
+from .utils import is_ajax
 
 def marketplace(request):
     vendor_list = Vendor.approved.all().order_by("vendor_name")
@@ -19,10 +22,61 @@ def marketplace(request):
     return render(request, 'marketplace/listings.html', context)
 
 
-def vendor_detail(request, vendor_slug):     
-    vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)     
-    categories = vendor.category.all().order_by("category_name").prefetch_related(
-        Prefetch("fooditems", queryset=FoodItem.objects.filter(is_available=True))
+def vendor_detail(request, vendor_slug):
+    vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+    categories = (
+        vendor.category.all()
+        .order_by("category_name")
+        .prefetch_related(
+            Prefetch("fooditems", queryset=FoodItem.objects.filter(is_available=True))
+        )
     )
-    context = {"vendor":vendor, "categories":categories}     
-    return render(request, 'marketplace/vendor_detail.html', context)
+    cart_items = Cart.objects.filter(user=request.user) if request.user.is_authenticated else None
+    context = {"vendor": vendor, "categories": categories, "cart_items": cart_items}
+    return render(request, "marketplace/vendor_detail.html", context)
+
+def add_to_cart(request, food_id):
+    if not request.user.is_authenticated:
+        return JsonResponse({"status": "failed", "message": "Please log in to proceed"})
+
+    if not is_ajax(request):
+        return JsonResponse({"status": "failed", "message": "Invalid request"})
+
+    food_item = FoodItem.objects.filter(id=food_id).first()
+    if not food_item:
+        return JsonResponse({"status": "failed", "message": "Food item does not exist"})
+
+    cart, created = Cart.objects.get_or_create(
+        user=request.user,
+        fooditem=food_item,
+    )
+    if not created:
+        cart.quantity += 1
+        cart.save()
+
+    return JsonResponse({"status": "success", "message": f"{food_item.food_title} added to cart successfully"})
+
+
+def remove_from_cart(request, food_id):
+    if not request.user.is_authenticated:
+        return JsonResponse({"status": "failed", "message": "Please log in to proceed"})
+
+    if not is_ajax(request):
+        return JsonResponse({"status": "failed", "message": "Invalid request"})
+
+    food_item = FoodItem.objects.filter(id=food_id).first()
+    if not food_item:
+        return JsonResponse({"status": "failed", "message": "Food item does not exist"})
+
+    cart = Cart.objects.filter(user=request.user, fooditem=food_item).first()
+
+    if cart is None:
+        return JsonResponse({"status": "success", "message": "Your cart is empty"})
+    
+    if cart and cart.quantity > 1:
+        cart.quantity -= 1
+        cart.save()
+    else:
+        cart.delete()
+
+    return JsonResponse({"status": "success", "message": f"{food_item.food_title} removed from cart successfully"})
