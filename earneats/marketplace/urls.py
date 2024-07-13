bc564142d3c8a563fefbf72f3a8a719f277b6a41
earnstein <bdamilola00@gmail.com>
2024-07-13 21:16:41 +0100

    add: delete cart url

--- earneats/marketplace/urls.py
+++ earneats/marketplace/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("<slug:vendor_slug>/", views.vendor_detail, name="vendor_detail"),
     path("add_to_cart/<int:food_id>/", views.add_to_cart, name="add_to_cart"),
     path("remove_from_cart/<int:food_id>/", views.remove_from_cart, name="remove_from_cart"),
+    path("delete_cart/<int:cart_id>/", views.delete_cart, name="delete_cart"),
 ]
