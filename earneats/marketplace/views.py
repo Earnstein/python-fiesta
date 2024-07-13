e8971dc92f7476909f111d4a8b3247e100febc87
earnstein <bdamilola00@gmail.com>
2024-07-13 02:46:29 +0100

    add: cart view / succes and failed constants

--- earneats/marketplace/views.py
+++ earneats/marketplace/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404
 from django.db.models import Prefetch
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from vendor.models import Vendor
-from menu.models import Category, FoodItem
+from menu.models import FoodItem
 from .models import Cart
 from .utils import is_ajax, get_total_cart_quantity
+
+SUCCESS = "success"
+FAILED = "failed"
+
 
 def marketplace(request):
     vendor_list = Vendor.approved.all().order_by("vendor_name")
@@ -38,14 +42,14 @@ def vendor_detail(request, vendor_slug):
 
 def add_to_cart(request, food_id):
     if not request.user.is_authenticated:
-        return JsonResponse({"status": "failed", "message": "Please log in to proceed"})
+        return JsonResponse({"status": FAILED, "message": "Please log in to proceed"})
 
     if not is_ajax(request):
-        return JsonResponse({"status": "failed", "message": "Invalid request"})
+        return JsonResponse({"status": FAILED, "message": "Invalid request"})
 
     food_item = FoodItem.objects.filter(id=food_id).first()
     if not food_item:
-        return JsonResponse({"status": "failed", "message": "Food item does not exist"})
+        return JsonResponse({"status": FAILED, "message": "Food item does not exist"})
 
     cart, created = Cart.objects.get_or_create(
         user=request.user,
@@ -55,30 +59,33 @@ def add_to_cart(request, food_id):
         cart.quantity += 1
         cart.save()
     total_quantity = get_total_cart_quantity(request.user)
-    return JsonResponse({"status": "success", "message": f"{food_item.food_title} added to cart successfully", "total_quantity": total_quantity, "qty": cart.quantity})
+    return JsonResponse({"status": SUCCESS, "message": f"{food_item.food_title} added to cart successfully", "total_quantity": total_quantity, "qty": cart.quantity})
 
 
 def remove_from_cart(request, food_id):
     if not request.user.is_authenticated:
-        return JsonResponse({"status": "failed", "message": "Please log in to proceed"})
+        return JsonResponse({"status": FAILED, "message": "Please log in to proceed"})
 
     if not is_ajax(request):
-        return JsonResponse({"status": "failed", "message": "Invalid request"})
+        return JsonResponse({"status": FAILED, "message": "Invalid request"})
 
     food_item = FoodItem.objects.filter(id=food_id).first()
     if not food_item:
-        return JsonResponse({"status": "failed", "message": "Food item does not exist"})
+        return JsonResponse({"status": FAILED, "message": "Food item does not exist"})
 
     cart = Cart.objects.filter(user=request.user, fooditem=food_item).first()
 
-    if not cart:
-        return JsonResponse({"status": "success", "message": "Your cart is empty"})
+    if not cart or cart.quantity == 0:
+        cart.delete()
+        return JsonResponse({"status": SUCCESS, "message": "Your cart is empty"})
     
-    if cart and cart.quantity > 1:
+    if cart and cart.quantity > 0:
         cart.quantity -= 1
         cart.save()
-    else:
-        cart.delete()
 
     total_quantity = get_total_cart_quantity(request.user)
-    return JsonResponse({"status": "success", "message": f"{food_item.food_title} removed from cart successfully", "total_quantity": total_quantity})
+    return JsonResponse({"status": SUCCESS, "message": f"{food_item.food_title} removed from cart successfully", "total_quantity": total_quantity, "qty": cart.quantity})
+
+
+def cart(request):
+    return render(request, "marketplace/cart.html")
