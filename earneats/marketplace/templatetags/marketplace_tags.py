ef6498a93b66cc329f7254d7dd442e25003e05ee
earnstein <bdamilola00@gmail.com>
2024-07-15 02:27:16 +0100

    add: cart_total template tag

--- earneats/marketplace/templatetags/marketplace_tags.py
+++ earneats/marketplace/templatetags/marketplace_tags.py
@@ -1,9 +1,8 @@
 from django import template
 from vendor.models import Vendor
-from ..utils import get_total_cart_quantity
+from ..utils import get_total_cart_quantity, get_total_cart_price
 
 register = template.Library()
-
 
 @register.simple_tag(name='active_vendors')
 def total_vendors():
@@ -14,3 +13,9 @@ def total_vendors():
 def total_cart_quantity(user):
     """Returns the total quantity of items in the user's cart."""
     return get_total_cart_quantity(user)
+
+
+@register.simple_tag(name="cart_total")
+def cart_total(user):
+    """Calculate and return the total cost of a user's cart."""
+    return get_total_cart_price(user)
