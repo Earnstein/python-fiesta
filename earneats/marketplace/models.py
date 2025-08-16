360345a94279ba9af740dcc188c1a136382eb617
earnstein <bdamilola00@gmail.com>
2025-08-16 20:59:46 +0100

    feat: add Tax model for managing tax details 💰

--- earneats/marketplace/models.py
+++ earneats/marketplace/models.py
@@ -29,3 +29,17 @@ class Cart(models.Model):
         return self.user.email
 
 
+
+
+class Tax(models.Model):
+    country = models.CharField(max_length=15,blank=True, null=True)
+    tax_type = models.CharField(max_length=20, unique=True, blank=True, null=True)
+    tax_percentage = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Tax Percentage (%)')
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'tax'
+        verbose_name_plural = 'taxes'
+
+    def __str__(self):
+        return self.tax_type
